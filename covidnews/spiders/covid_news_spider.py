d08d7323e783ba14a75f3cd14036aa3c79e7f15f
Phung Cheng Fei <feiphung@hotmail.com>
2023-09-05 18:07:20 +0800

    Major code modifications to allow correct article content retrieval process

--- covidnews/spiders/covid_news_spider.py
+++ covidnews/spiders/covid_news_spider.py
@@ -11,7 +11,7 @@ import requests
 #search_keywords = ['covid','virus','pandemic','vaccine','corona','vaccination','circuit breaker','SARS-CoV-2']
 search_keywords = ['covid','pandemic','vaccine','coronavirus','vaccination','SARS-CoV-2']
 
-# Whether to brute-force search across the entire website hierarchy
+# Whether to brute-force search across the entire website hierarchy, due to robots.txt restriction
 SEARCH_ENTIRE_WEBSITE = 1
 
 # Whether to skip cdx search
@@ -23,8 +23,8 @@ class CovidNewsSpider(scrapy.Spider):
 
     start_urls = [
         #'https://web.archive.org/'
-        'https://www.straitstimes.com/'
-        #'https://www.channelnewsasia.com/'
+        #'https://www.straitstimes.com/'
+        'https://www.channelnewsasia.com/'
         #'https://www.channelnewsasia.com/search?q=covid'  # [scrapy.downloadermiddlewares.robotstxt] DEBUG: Forbidden by robots.txt:
         #'https://www.straitstimes.com/search?searchkey=covid'  # Forbidden by https://www.straitstimes.com/robots.txt
     ]
@@ -216,8 +216,8 @@ class CovidNewsSpider(scrapy.Spider):
                         headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
                     )
 
-    def get_next_page(self, response):
-        print("inside get_next_page(), response.url = ", response.url)
+    def get_next_pages(self, response):
+        print("inside get_next_pages(), response.url = ", response.url)
 
         if 'channelnewsasia' in response.url:
             more_links = response.css('a::attr(href)').getall()  # Replace with the correct CSS selector
@@ -267,35 +267,13 @@ class CovidNewsSpider(scrapy.Spider):
             articles = []
 
         for article in articles:
-            for parsed_article in self.parse_article(article, response):
-                title = parsed_article['title']
-                link = parsed_article['link']
-                body = parsed_article['body']
-
-            if link:
-                if "javascript" in link or "mailto" in link or "play.google.com" in link or "apps.apple.com" in link or \
-                    "www.channelnewsasia.com/watch" in link or "cnaluxury.channelnewsasia.com" in link or \
-                    "www.channelnewsasia.com/about-us" in link or "whatsapp://" in link:
-                    # Skip links
-                    continue
-
-            print("article = ", article)
-
-            if (title != None and any(keyword in title.lower() for keyword in search_keywords)) or (body != None and any(keyword in body.lower() for keyword in search_keywords)):
-                # Create a unique filename for each URL by removing the 'http://', replacing '/' with '_', and adding '.html'
-                filename = link.replace('http://', '').replace('/', '_') + '.html'
-                print("filename = ", filename)
-
-                # Write the entire body of the response to a file
-                #with open("/home/phung/covidnews_result/"+filename, 'wb') as f:
-                with open(filename, 'wb') as f:
-                    f.write(response.body)
+            yield from self.parse_article(article, response)
 
         next_pages = None
 
         if not INTERNETARCHIVE_FULL_TEXT:
-            # Get the next page URLs and yield new requests
-            next_pages = self.get_next_page(response)
+            # Get the next pages URLs and yield new requests
+            next_pages = self.get_next_pages(response)
 
         if next_pages is not None:
             next_pages = list(next_pages)
@@ -352,7 +330,7 @@ class CovidNewsSpider(scrapy.Spider):
             else:
                 print("Already downloaded and extracted the FULL_TEXT archive.org article")
                 # (be aware of compressed zip file format containing multiple djvu.txt.html webpage files,
-                # OR Microsoft Word document)
+                # OR Microsoft Word OR Adobe PDF document)
                 return response.css('*')
 
 
@@ -364,10 +342,8 @@ class CovidNewsSpider(scrapy.Spider):
         elif 'archive.org' in response.url:
             return 'archive'
 
+
     def parse_article(self, article, response):
-        title = "testing123"
-        link = "testing123"
-        date = "testing123"
 
         if 'channelnewsasia' in response.url:
             title = article.css('title::text').get() or \
@@ -376,46 +352,90 @@ class CovidNewsSpider(scrapy.Spider):
                     article.css('div.quick-link::attr(data-heading)').get() or \
                     article.css('meta[property="og:title"]::attr(content)').get() or \
                     article.css('meta[name="twitter:title"]::attr(content)').get()
+            date = article.css('time.entry-date::text').get() or article.css('div.list-object__datetime-duration span::text').get()
+
             link = article.css('h1.entry-title a::attr(href)').get() or \
                     article.css('h6.list-object__heading a::attr(href)').get() or \
                     article.css('div.quick-link::attr(data-link_absolute)').get()
-            date = article.css('time.entry-date::text').get() or article.css('div.list-object__datetime-duration span::text').get()
-            body = article.css('div.article p::text').getall() or \
-                   article.css('div.text-long').getall() or \
-                   response.css('main#maincontent > div.container.container-ia > pre::text').getall()
-            body = '\n'.join(body)
 
         elif 'straitstimes' in response.url:
             title = article.css('h5.card-title a::text').get()
-            link = article.css('a::attr(href)').get()
             date = article.css('time::text').get()
-            body = article.xpath('//p[not(@*)]/text()').getall()
-            #body = [p.css('::text').get() for p in article.css('p')]
+
+            link = article.css('a::attr(href)').get()
+
+        elif 'archive.org' in response.url:
+            title = article.css('title::text').get()
+            date = article.xpath('//meta[@name="date"]/@content').get()
+
+            link = response.css('a.format-summary.download-pill:contains("FULL TEXT")::attr(href)').get()
+
+        #print(f"link : {link} is of type : {type(link)}")
+
+        article_url = link
+
+        yield SplashRequest(
+            url=response.urljoin(article_url),
+            callback=self.get_article_content,
+            meta={'title': title, 'date': date},  # Pass additional data here
+            args={'wait': 0.5},
+            headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,      like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        )
+
+
+    def get_article_content(self, response):
+        # retrieves article's detailed title and body properly
+
+        title = response.meta['title']  # Access the additional data here
+        date = response.meta['date']  # Access the additional data here
+
+        if 'channelnewsasia' in response.url:
+            body = response.xpath('//p[not(@*)]//descendant-or-self::node()/text()').getall()
+            body = '\n'.join(body)
+            #body = response.css('div.article p::text').getall() or \
+            #       response.css('div.text-long').getall() or \
+            #       response.css('main#maincontent > div.container.container-ia > pre::text').getall()
+            #body = '\n'.join(body)
+
+        elif 'straitstimes' in response.url:
+            body = response.xpath('//p[not(@*)]/text()').getall()
             body = '\n'.join(body)
 
         elif 'archive.org' in response.url:
-            print("we are here, nice !!!")
-            title = article.css('title::text').get()
-            link = response.css('a.format-summary.download-pill:contains("FULL TEXT")::attr(href)').get()
-            date = article.xpath('//meta[@name="date"]/@content').get()
-            body = article.css('div.article p::text').getall() or \
-                   article.css('div.text-long').getall() or \
+            body = response.css('div.article p::text').getall() or \
+                   response.css('div.text-long').getall() or \
                    response.css('main#maincontent > div.container.container-ia > pre::text').getall()
             body = '\n'.join(body)
 
         else:
-            title = None
-            link = None
-            date = None
             body = None
 
-        print(f"inside parse_article(), parent_url = {response.url} , article_url = {link} , title = {title}, date = {date}, body = {body}")
+        link = response.url
+
+        print(f"inside parse_article(), article_url = {link} , title = {title}, date = {date}, body = {body}")
+
+        self.write_to_local_data(link, title, body, response)
 
         yield {
             'title': title,
             'link': link,
             'date': date,
             'body': body,
-            'excerpt': article.css('p::text').get(),
+            #'excerpt': article.css('p::text').get(),
             'source': self.get_source(response)
         }
+
+
+    def write_to_local_data(self, link, title, body, response):
+        if (title != None and any(keyword in title.lower() for keyword in search_keywords)) or (body != None and any(keyword in body.lower() for keyword in search_keywords)):
+            # Create a unique filename for each URL by removing the 'http://', replacing '/' with '_', and adding '.html'
+            filename = link.replace('http://', '').replace('/', '_') + '.html'
+            print("filename = ", filename)
+
+            # Write the entire body of the response to a file
+            #with open("/home/phung/covidnews_result/"+filename, 'wb') as f:
+            with open(filename, 'wb') as f:
+                f.write(response.body)
+
+        return None
+
